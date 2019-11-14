ab827ecadbc65336fba74fe1d00f47d747652c25
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-11-14 10:03:15 -0500

    feat: Added google map to contact page

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -10,6 +10,7 @@
         the_post(); ?>
         
     <h2><?php the_title(); ?></h2>
+    
     <?php the_content(); ?>
     
     <!-- Loop ends -->
@@ -20,21 +21,23 @@
     <?php else : ?>
         <p>No posts found</p>
     <?php endif;?>
+
+    
     
     <form action="" class="contactForm" id="contact-form">
-        <label for="name">NAME</label>
-        <input type="text" id="name">
+        <label for="name"> <p class="form-label novecento-web" >NAME<span class="red" >*</span></p></label>
+        <input type="text" id="name" class="input-field">
 
-        <label for="email">EMAIL</label>
-        <input type="email" id="email">
+        <label for="email"> <p class="form-label novecento-web">EMAIL<span class="red">*</span></p> </label>
+        <input type="email" id="email" class="input-field">
 
-        <label for="subject">SUBJECT</label>
-        <input type="text" id="subject">
+        <label for="subject"> <p class="form-label novecento-web">SUBJECT<span class="red">*</span></p></label>
+        <input type="text" id="subject" class="input-field">
 
-        <label for="contact-form">MESSAGE</label>
+        <label for="contact-form"><p class="form-label novecento-web">MESSAGE<span class="red">*</span></p></label>
 
-        <textarea name="" id="contact-form" cols="30" rows="10"></textarea>
-        <input type="submit">
+        <textarea name="" id="contact-form" cols="30" rows="10" class="input-message"></textarea>
+        <input class="contact-button" type="submit" value="SUBMIT">
 
         </form>
 </div>
