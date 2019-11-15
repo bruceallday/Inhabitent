5dd8459af376f038d9f38c2d5363c900262662e3
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-11-15 09:27:45 -0500

    style: Added grid to single.php

--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -1,27 +1,35 @@
 <?php get_header(); ?>
 
-<?php if( have_posts() ) :
-//The WordPress Loop: loads post content 
-    while( have_posts() ) :
+<section class="single-post-page">
+    <div>
+         <?php if( have_posts() ) :
+        //The WordPress Loop: loads post content 
+        while( have_posts() ) :
         the_post(); ?>
     
-    <h2><?php the_title(); ?></h2>
-    <?php the_post_thumbnail('large');?>
-    <?php the_content(); ?>
-    <?php comment_form(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_post_thumbnail('large');?>
+        <?php the_content(); ?>
 
     <!-- Loop ends -->
-    <?php endwhile;?>
+        <?php endwhile;?>
 
-    <?php the_posts_navigation();?>
+        <?php the_posts_navigation();?>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif;?>
+        <?php else : ?>
+            <p>No posts found</p>
+        <?php endif;?>
+
+    </div>
+   
 
      <?php if ( comments_open() || get_comments_number() ) :
      comments_template();
     endif; ;?>
 
+<?php get_sidebar();?>
+
+
+</section>
 
 <?php get_footer();?>
